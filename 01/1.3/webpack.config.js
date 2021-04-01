6dcd16dd59fdd103e7960f4743d9020852dc9019
IuriiKyrylovskyi <iurii712@ukr.net>
2021-04-01 20:21:39 +0300

    1.3

--- 01/1.3/webpack.config.js
+++ 01/1.3/webpack.config.js
@@ -1,7 +1,8 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const webpack = require('webpack');
+const webpack = require("webpack");
+const path = require("path");
 
 module.exports = (env, argv) => {
   const isProduction = argv.mode === "production";
