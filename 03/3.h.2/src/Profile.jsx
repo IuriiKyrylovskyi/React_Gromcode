b6475cb5f84aa82b585ddb56245ada9406832c25
IuriiKyrylovskyi <iurii712@ukr.net>
2021-04-02 15:27:32 +0300

    3.h.2

---
+++ 03/3.h.2/src/Profile.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import moment from 'moment'
+
+const userData = {
+    firstName: 'John',
+    lastName: 'Doe',
+    birthDate: '1991-01-17T11:11:11.819Z',
+    birthPlace: 'London'
+};
+
+const bithDateFormatter = date => moment(date).locale("en-ie").format("D MMM YY")
+
+const Profile = () => {
+	return (
+		<div className='info'>
+			<p>
+				{`${userData.firstName} ${userData.lastName}`}
+			</p>
+			<p>
+				{`Was born ${bithDateFormatter(userData.birthDate)} in ${userData.birthPlace}`}
+			</p>
+		</div>
+	)
+};
+
+export default Profile
---
+++ 03/3.h.2/src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.scss'
+import Profile from './Profile'
+
+const rootElem = document.querySelector('#root')
+
+ReactDOM.render(
+	<Profile />,
+	rootElem
+)
