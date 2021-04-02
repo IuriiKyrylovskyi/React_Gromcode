e63888327cd1c708474e520491fed93881895538
IuriiKyrylovskyi <iurii712@ukr.net>
2021-04-02 19:22:26 +0300

    4.1

---
+++ 04/4.1/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Counter from './Counter'
+
+const App = () => {
+	return (
+		<>
+			<Counter start={25} interval={1000} /> 
+			<Counter start={12} interval={100} /> 
+			<Counter start={-21} interval={500} />
+		</>
+	)
+}
+
+export default App;
