16dd140e4f8b31a6b604d350de753fc58291aa00
IuriiKyrylovskyi <iurii712@ukr.net>
2021-04-02 12:23:57 +0300

    3.3

---
+++ 03/3.3/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './styles.scss'
+import Comment from './Comment'
+
+const rootElem = document.querySelector('#root')
+
+ReactDOM.render(<Comment />, rootElem)
