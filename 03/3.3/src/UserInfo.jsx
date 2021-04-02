058fe983909962a95dc63c9acc2a32d68572a7ae
IuriiKyrylovskyi <iurii712@ukr.net>
2021-04-02 13:25:36 +0300

    3.3

--- 03/3.3/src/UserInfo.jsx
+++ 03/3.3/src/UserInfo.jsx
@@ -2,17 +2,10 @@ import React from 'react'
 import './user-info.scss'
 import Avatar from './Avatar'
 
-const user = {
-	name: 'Me',
-	avatarUrl: 'https://avatars1.githubusercontent.com/'
-}
-
-
 const UserInfo = props => {
 	return (
 		<div className="user-info">
 			<Avatar
-				// user={props.user}
 				avatarUrl={props.user.avatarUrl}
 				name={props.user.name}
 			/>
--- 03/3.3/src/index.jsx
+++ 03/3.3/src/index.jsx
@@ -5,9 +5,14 @@ import Comment from './Comment'
 
 const rootElem = document.querySelector('#root')
 
+const author = {
+	name: 'IuriiKyrylovskyi',
+	avatarUrl: 'https://avatars.githubusercontent.com/u/65412895?s=60&v=4'
+}
+
 ReactDOM.render(
 	<Comment
-		author=''
+		user={author}
 		text='Not bad'
 		date={Date.now()}
 	/>,
