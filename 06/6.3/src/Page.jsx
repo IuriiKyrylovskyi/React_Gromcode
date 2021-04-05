cd410be011026cdb16c2ebd347d7c1861c680fe2
IuriiKyrylovskyi <iurii712@ukr.net>
2021-04-05 16:47:56 +0300

    6.3

--- 06/6.3/src/Page.jsx
+++ 06/6.3/src/Page.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Message from './Message';
 
+const text1 = 'Hello world!';
+const text2 = 'Another exciting text.'
+
 class Page extends React.Component{
 	constructor(props) {
 		super(props);
@@ -10,18 +13,24 @@ class Page extends React.Component{
 		}
 	}
 
+	setText = text => {
+		this.setState({
+			text: text
+		})
+	}
+
 	render() {
 		return (
 			<div className="page">
 				<Message text={this.state.text} />
 				<div className="actions">
-					<button className="btn">
+					<button className="btn" onClick={() => this.setText(text1)}>
 						Text 1
 					</button>
-					<button className="btn">
+					<button className="btn" onClick={() => this.setText(text2)}>
 						Text 2
 					</button>
-					<button className="btn">
+					<button className="btn" onClick={() => this.setText('')}>
 						Clear
 					</button>
 				</div>		
