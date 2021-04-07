82bb26e5170e54a27514d0c9f84cd502e0b0a9a3
IuriiKyrylovskyi <iurii712@ukr.net>
2021-04-07 19:28:21 +0300

    7.add.1

--- 07/7.add.1/src/Pagination.jsx
+++ 07/7.add.1/src/Pagination.jsx
@@ -7,14 +7,14 @@ const Pagination = ({ goPrev, goNext, currentPage, totalItems, itemsPerPage}) =>
 	return (
 		<div className="pagination">
 			{isPrevPageAvailable === 1
-				? <button className="btn" disable={'true'}></button>
+				? <button className="btn" disabled={true}></button>
 				: <button className="btn" onClick={goPrev}>←</button>
 			}
 
 			<span className="pagination__page">{currentPage}</span>
 
 			{isNextPageAvailable === Math.ceil(totalItems / itemsPerPage)
-				? <button className="btn" disable={'true'}></button>
+				? <button className="btn" disabled={true}></button>
 				: <button className="btn" onClick={goNext}>→</button>
 			}
 		</div>
