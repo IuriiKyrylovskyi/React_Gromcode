0718594db30b1921a4c22c755c0288522f333486
IuriiKyrylovskyi <iurii712@ukr.net>
2021-04-07 21:41:02 +0300

    8.1

--- 08/8.1/src/Life.jsx
+++ 08/8.1/src/Life.jsx
@@ -10,11 +10,11 @@ class Life extends Component {
 		console.log('componentDidMount: API calls, subscriptions');
 	}
 
-	shouldComponentUpdate() {
+	shouldComponentUpdate(nextProps, nextState) {
 		console.log('shouldComponentUpdate(nextProps, nextState): decide to render or not to render');
 	}
 
-	componentDidUpdate() {
+	componentDidUpdate(prevProps, prevState) {
 		console.log('componentDidUpdate(prevProps, prevState): some updates based on new props');
 	}
 
