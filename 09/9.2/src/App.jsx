1d4545614292a33eb0bff0b246576c8bb78f186a
IuriiKyrylovskyi <iurii712@ukr.net>
2021-04-08 22:40:46 +0300

    9.2

---
+++ 09/9.2/src/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import UserForm from './UserForm';
+
+class App extends Component {
+
+	this.props.createUser{
+		console.log();
+	}
+
+	return (
+		<UserForm />
+	)
+}
+
+export default App;
--- 09/9.2/src/UserForm.jsx
+++ 09/9.2/src/UserForm.jsx
@@ -1,30 +1,73 @@
 import React, { Component } from 'react';
 
 class UserForm extends Component{
+	state = {
+		name: '',
+		student: '',
+		occupation: '',
+		about: '',
+	}
+
+	handleChange = e => {
+		const { name, value } = e.target;
+
+		this.setState({
+			[name]: value
+		})
+	}
+
+	handleSubmit = e => {
+		e.preventDefault();
+	}
+
 	render() {
 		return (
-			<form className="login-form">
+			<form className="login-form" onSubmit={handleSubmit}>
 				<h1 className="form-title">Profile</h1>
 				<div className="form-control">
-					<label className="form-label" for="name">Name</label>
-					<input className="form-input" type="text" id="name" name="name" value=""/>
+					<label className="form-label" htmlFor="name">Name</label>
+					<input
+						className="form-input"
+						type="text"
+						id="name"
+						name="name"
+						onChange={this.handleChange}
+						value={this.state.name}
+					/>
 				</div>
 				<div className="form-control">
-					<label className="form-label" for="student">Student</label>
-					<input className="form-input" type="checkbox" id="student" name="student"/>
+					<label className="form-label" htmlFor="student">Student</label>
+					<input
+						className="form-input"
+						type="checkbox"
+						id="student"
+						name="student"
+						onChange={this.handleChange}
+						value={this.state.student}
+					/>
 				</div>
 				<div className="form-control">
-					<label className="form-label" id="occupation" for="occupation">Occupation</label>
-					<select name="occupation" className="form-input">
+					<label className="form-label" id="occupation" htmlFor="occupation">Occupation</label>
+					<select
+						name="occupation"
+						className="form-input"
+						onChange={this.handleChange}
+						value={this.state.occupation}
+					>
 						<option value="london">London</option>
 						<option value="new-york">New York</option>
-						<option value="coconut">Sidney</option>
-						<option value="mango">Berlin</option>
+						<option value="sidney">Sidney</option>
+						<option value="berlin">Berlin</option>
 					</select>
 				</div>
 				<div className="form-control">
-					<label className="form-label" id="about" for="about">About</label>
-					<textarea name="about" className="form-input"></textarea>
+					<label className="form-label" id="about" htmlFor="about">About</label>
+					<textarea
+						name="about"
+						className="form-input"
+						onChange={this.handleChange}
+						value={this.state.about}
+					/>
 				</div>
 				<button className="submit-button" type="submit">Submit</button>
 			</form>			
