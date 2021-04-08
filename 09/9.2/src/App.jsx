e06db42dd61e3599c2e37f90f057fc5313a65cb3
IuriiKyrylovskyi <iurii712@ukr.net>
2021-04-09 00:56:29 +0300

    9.2

--- 09/9.2/src/App.jsx
+++ 09/9.2/src/App.jsx
@@ -2,15 +2,15 @@ import React, { Component } from 'react';
 import UserForm from './UserForm';
 
 class App extends Component {
-	createUser = user => {
-		console.log(user);
-	}
+  createUser = user => {
+    console.log(user);
+  }
 
-	render() {
-		return (
-			<UserForm onSubmit={this.createUser}/>
-		)
-	}
+  render() {
+    return (
+      <UserForm onSubmit={this.createUser}/>
+    )
+  }
 }
 
 export default App;
