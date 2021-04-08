1fb2beed3ce0a0a22027fe071bc18476b0e20c8a
IuriiKyrylovskyi <iurii712@ukr.net>
2021-04-09 00:59:28 +0300

    9.1

--- 09/9.1/src/Search.jsx
+++ 09/9.1/src/Search.jsx
@@ -1,45 +1,45 @@
 import React, { Component } from 'react';
 
 class Search extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			value: '',
-		}
-	}
+    this.state = {
+      value: '',
+    }
+  }
 
-	handleChange = e => {
-		this.setState({
-			value: e.target.value
-		});
-	}
-	
-	search = e => {
-		e.preventDefault();
-		alert(`Search text: ${this.state.value}`);
-	}
-	
-	render() {
-		return (
-			<form className="search"
-				onSubmit={this.search}
-			>
-				<input
-					type="text"
-					value={this.state.value}
-					onChange={this.handleChange}
-					className="search__input"
-				/>
-				<button
-					className="search__button"
-					type="submit"
-				>
-					Search
-				</button>
-			</form>
-		)
-	}
+  handleChange = e => {
+    this.setState({
+      value: e.target.value
+    });
+  }
+  
+  search = e => {
+    e.preventDefault();
+    alert(`Search text: ${this.state.value}`);
+  }
+  
+  render() {
+    return (
+      <form className="search"
+        onSubmit={this.search}
+      >
+        <input
+          type="text"
+          value={this.state.value}
+          onChange={this.handleChange}
+          className="search__input"
+        />
+        <button
+          className="search__button"
+          type="submit"
+        >
+          Search
+        </button>
+      </form>
+    )
+  }
 }
 
 export default Search;
