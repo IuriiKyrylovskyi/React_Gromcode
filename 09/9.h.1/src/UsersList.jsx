74e8ed49a1df3883af123a32546d9a290d609923
IuriiKyrylovskyi <iurii712@ukr.net>
2021-04-09 22:51:23 +0300

    9.h.1

--- 09/9.h.1/src/UsersList.jsx
+++ 09/9.h.1/src/UsersList.jsx
@@ -13,11 +13,7 @@ class UsersList extends Component {
  
   handleFilterOnChange = e => {
     if (e.target.value.trim() === "") {
-      return this.setState({
-                users: this.props.users ,
-                count: this.props.users.length,
-                text: e.target.value,
-      })
+      return
     }
     const filteredUsers = this.props.users
       .filter(user =>
@@ -25,10 +21,10 @@ class UsersList extends Component {
         .toLowerCase()
         .includes(e.target.value.toLowerCase()))
     
-      this.setState({
-          users: filteredUsers ,
-          count: filteredUsers.length,
-          text: e.target.value,
+    this.setState({
+      users: filteredUsers ,
+      count: filteredUsers.length,
+      text: e.target.value,
     })
   }
 
