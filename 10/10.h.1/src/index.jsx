59c1a7590df821c04f3bf4dfaf1e2fde33493ed2
IuriiKyrylovskyi <iurii712@ukr.net>
2021-04-12 23:22:24 +0300

    10.h.1

--- 10/10.h.1/src/index.jsx
+++ 10/10.h.1/src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './App';
+import Calculator from './Calculator';
 
 const rootElem = document.querySelector('#root');
 
-ReactDOM.render(<App />, rootElem);
+ReactDOM.render(<Calculator />, rootElem);
