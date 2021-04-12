2d98c4c74a49ea83191d80bbd50143e0710920c0
IuriiKyrylovskyi <iurii712@ukr.net>
2021-04-12 22:25:47 +0300

    10.2

--- 10/10.2/src/ProductsList.jsx
+++ 10/10.2/src/ProductsList.jsx
@@ -8,12 +8,13 @@ const ProductsList = ({cartItems}) => {
   return (
     <div className="products">
       <ul className="products__list">
-        {cartItems.map(({ id, name, price }) => 
-          <li key={id} className="products__list-item">
-            <span className="products__item-name">{name}</span>
-            <span className="products__item-price">{`$${price}`}</span>
-          </li>
-        )}
+        {
+          cartItems.map(({ id, name, price }) =>
+            (<li key={id} className="products__list-item">
+              <span className="products__item-name">{name}</span>
+              <span className="products__item-price">{`$${price}`}</span>
+            </li>))
+        }
       </ul>
       <div className="products__total">{`Total: $${total}`}</div>
     </div>
