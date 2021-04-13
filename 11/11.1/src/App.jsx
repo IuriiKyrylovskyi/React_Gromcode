fe30c84ad96f64757937f0e88a7ec8af7834f851
IuriiKyrylovskyi <iurii712@ukr.net>
2021-04-13 15:11:58 +0300

    11.1

---
+++ 11/11.1/src/App.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import Numbers from './Numbers';
+import EvenNumbers from './EvenNumbers';
+import OddNumbers from './OddNumbers';
+
+class App extends Component{
+  render() {
+    return (
+      <div className="app">
+        <OddNumbers
+          title={'Odd number.'}
+          number={this.state.number}
+        />
+        <EvenNumbers
+          title={'Even number.'}
+          number={this.state.number}
+        />
+        <Numbers
+          title={this.state.title}
+          number={this.state.number}
+        />
+        <Numbers
+          title={this.state.title}
+          number={this.state.number}
+        />
+      </div>
+    )
+  }
+}
+
+export default App;
