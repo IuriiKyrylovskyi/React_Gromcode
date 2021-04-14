8c4bef532574b95fe0c3edb54330ba86e52a82e8
IuriiKyrylovskyi <iurii712@ukr.net>
2021-04-14 16:32:53 +0300

    11.add.1

--- 11/11.add.1/src/withDataLoader.jsx
+++ 11/11.add.1/src/withDataLoader.jsx
@@ -1,5 +1,29 @@
+import React, { Component } from 'react';
+import Spinner from './Spinner';
+
 export const withDataLoader = data => {
-  fetch(data)
-  .then(response => response.json())
+  return class extends Component{
+    state = {
+      data,
+    }
+
+    handleChange = () => {
+      const loadedData = fetch(data).then(response => response.json())
+      this.setState({
+        data: loadedData,
+      })
+    }
+    
+    render() {
+      if (!data) {
+        return (
+          <Spinner />
+        )
+      }
+      return this.state.data;
+    }
+  }
 }
+
+// export withDataLoader;
 
