6126926e814530150381046a98c8df7b91bd4d77
IuriiKyrylovskyi <iurii712@ukr.net>
2021-04-23 12:51:31 +0300

    fetch

---
+++ 15.calendar_react/src/gateway/gateway.js
@@ -0,0 +1,35 @@
+const baseUrl = "https://608294355dbd2c0017579ede.mockapi.io/events";
+
+export const fetchEvents = () => {
+  return fetch(baseUrl).then((response) => {
+    if (response.ok) {
+      return response.json();
+    }
+  });
+}
+
+export const createEvent = eventData => {
+  return fetch(baseUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8;'
+    },
+    body: JSON.stringify(eventData),
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Failed to create event');
+    }
+  })
+}
+
+export const deleteEvent = eventId => {
+  return fetch(`${baseUrl}/${eventId}`, {
+    method: 'DELETE',
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("Failed to delete event");
+    }
+  })
+}
