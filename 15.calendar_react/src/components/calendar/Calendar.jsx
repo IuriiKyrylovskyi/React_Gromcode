71e6222503319a8683badf3c608100ec2cbf31b8
IuriiKyrylovskyi <iurii712@ukr.net>
2021-04-25 00:45:16 +0300

    15

--- 15.calendar_react/src/components/calendar/Calendar.jsx
+++ 15.calendar_react/src/components/calendar/Calendar.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
-
+import { useGlobalContext } from "../../context";
 import Navigation from "./../navigation/Navigation";
 import Week from "../week/Week";
 import Sidebar from "../sidebar/Sidebar";
-import { fetchEventsList, createEvent, deleteEvent } from '../../gateway/gateway.js'
+import { fetchEventsList, createEvent, deleteEvent } from "../../gateway/gateway.js";
 // import events from "../../gateway/events";
 
 import "./calendar.scss";
@@ -11,12 +11,12 @@ import "./calendar.scss";
 class Calendar extends Component {
   state = {
     events: [],
-    title: '',
-    dateFrom: '', 
-    dateTo: '', 
-    description: '',
+    title: "",
+    dateFrom: "",
+    dateTo: "",
+    description: "",
   };
-  
+
   componentDidMount() {
     this.fetchEvents();
   }
@@ -31,14 +31,13 @@ class Calendar extends Component {
   }
 
   fetchEvents = () => {
-    fetchEventsList()
-      .then(eventsList => {
-        console.log(eventsList);
-        this.setState({
-          events: eventsList,
-        })
-      })
-  }
+    fetchEventsList().then((eventsList) => {
+      console.log(eventsList);
+      this.setState({
+        events: eventsList,
+      });
+    });
+  };
 
   // handleEventCreate = () => {
   //   const { title, dateFrom, dateTo, description } = this.state;
@@ -54,26 +53,20 @@ class Calendar extends Component {
   //     .then(() => this.fetchEvents())
   // }
 
-  handleEventDelete = id => {
-    deleteEvent(id)
-      .then(() => this.fetchEvents())
-  }
-
+  handleEventDelete = (id) => {
+    deleteEvent(id).then(() => this.fetchEvents());
+  };
 
   render() {
     const { weekStartDate, weekDates } = this.props;
-
+console.log(weekDates);
     return (
       <section className="calendar">
         <Navigation weekDates={weekDates} />
         <div className="calendar__body">
           <div className="calendar__week-container">
             <Sidebar />
-            <Week
-              weekDates={weekDates}
-              events={this.state.events}
-              weekStartDate={weekStartDate}
-            />
+            <Week weekDates={weekDates} events={this.state.events} weekStartDate={weekStartDate} />
           </div>
         </div>
       </section>
