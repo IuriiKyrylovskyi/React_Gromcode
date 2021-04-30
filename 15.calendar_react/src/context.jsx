0ffde83831d4b8ffff0edbf704ca1cae15076fe7
IuriiKyrylovskyi <iurii712@ukr.net>
2021-04-30 22:24:16 +0300

    inputs

--- 15.calendar_react/src/context.jsx
+++ 15.calendar_react/src/context.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from "react";
-import moment from 'moment';
+import moment from "moment";
 
 const AppContext = React.createContext();
 
@@ -7,39 +7,37 @@ const AppProvider = ({ children }) => {
   const [isOpen, setIsOpen] = useState(false);
   const [isEvent, setEvent] = useState(false);
 
-  const [dateInput, setDateInput] = useState('');
-  const [startTimeInput, setStartTimeInput] = useState('');
-  const [endTimeInput, setEndTimeInput] = useState('');
-
-
+  const [dateInput, setDateInput] = useState("");
+  const [startTimeInput, setStartTimeInput] = useState("");
+  const [endTimeInput, setEndTimeInput] = useState("");
 
   const onCloseModal = () => {
     setIsOpen(false);
   };
 
   const onOpenModal = (e) => {
-    if (e.target.closest(".event")) {
-      setDateInput('');
-      setStartTimeInput('');
-      setEndTimeInput('');
-      
-      setEvent(!isEvent);
-      return;
-    }
-    if (isEvent) {
+    if (isEvent || e.target.closest(".event")) {
       return; // setEvent(false);
     }
-    // if (!isEvent && e.target.closest(".calendar__time-slot")) {
-    //   setEvent(false);
-    //   setIsOpen(true);
-    //   return;
-    // }
-    setDateInput(moment().format("YYYY-MM-DD"));
-    setStartTimeInput(moment().format("HH:mm"));
-    setEndTimeInput(moment().format("HH:mm"));
-    
-    setEvent(false);
-    setIsOpen(true);
+    if (e.target.className === "calendar__time-slot") {
+      setDateInput("");
+      setStartTimeInput("");
+      setEndTimeInput("");
+
+      setEvent(false);
+      setIsOpen(true);
+      return;
+    }
+   
+    if (!e.target.closest("event")) {
+      console.log(e.target.className);
+      setDateInput(moment().format("YYYY-MM-DD"));
+      setStartTimeInput(moment().format("HH:mm"));
+      setEndTimeInput(moment().format("HH:mm"));
+
+      setEvent(false);
+      setIsOpen(true);
+    }
   };
 
   return (
